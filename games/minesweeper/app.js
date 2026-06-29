252cd3d5ea988bd0a98f930b109fd3a0a81311b9
nuo534202 <t330026229@mail.uic.edu.cn>
2026-06-30 01:14:26 +0800

    refactor: change minesweeper expert mode

--- games/minesweeper/app.js
+++ games/minesweeper/app.js
@@ -5,7 +5,7 @@
     var DIFFICULTIES = {
         beginner:     { rows: 9,  cols: 9,  mines: 10 },
         intermediate: { rows: 16, cols: 16, mines: 40 },
-        expert:       { rows: 16, cols: 30, mines: 99 }
+        expert:       { rows: 20, cols: 20, mines: 100 }
     };
 
     // Game state
